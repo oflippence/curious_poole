0246b302b16be09f1dcdfc4bb24d85fcc92226d2
nealereynolds <mail@nealereynolds.co.uk>
2015-01-25 19:12:34 +0000

    added in huge inc fullpage slider

--- _js/sidebarEffects.js
+++ _js/sidebarEffects.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -78,12 +78,3 @@
 	init();
 
 })();
-
-(function($){
-
-$('#section-1').waypoint(function(direction) {
-			alert('test');
-		});
-})(jQuery);
-
-
